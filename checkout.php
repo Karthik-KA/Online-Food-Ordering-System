6c63a9d52a7098c1a5ad4ae514ef74c863330d71
Karthik-KA <karthik.ka2001@gmail.com>
2023-06-11 18:43:51 +0530

    final commit

--- checkout.php
+++ checkout.php
@@ -174,8 +174,8 @@ else{
                 
       
 
-     <!-- Bootstrap core JavaScript
-    ================================================== -->
+     <!-- Bootstrap core JavaScript-->
+
     <script src="js/jquery.min.js"></script>
     <script src="js/tether.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
